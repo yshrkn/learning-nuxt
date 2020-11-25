b4ab9cc3565a2d56fcc6ff9b7bcf667e8d2f5c28
Ken Yoshihara <yshrkn@gmail.com>
2020-11-25 23:47:55 +0900

    css module test

---
+++ nuxt-projects/nuxt-spa/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button :class="[$style.button, $style.red]">
+    {{ label }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+  }
+}
+</script>
+
+<style lang="scss" module>
+@import '~/components/Button.scss';
+</style>
